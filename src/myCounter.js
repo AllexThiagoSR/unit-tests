7dd453d7677817c298468d8f201d1bcfd2b1351a
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-24 13:11:25 -0300

    Requisito 8: Corrige a função myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let count = 0; count <= 3; count += 1) {
+    myArray.push(count);
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-
+// console.log(myCounter());
 module.exports = myCounter;
