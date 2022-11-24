28d83405c49bc4f87eab5974b2ebe9cf936da1d5
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-24 11:07:14 -0300

    Requisito 3: Implementa a função vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,10 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
-
+const vqv = (name, age) => {
+  if (!name || !age) return;
+  if (typeof name !== 'string' || Number.isNaN(age)) return;
+  return `Oi, meu nome é ${name}!\nTenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+};
+// console.log(vqv('Tunico', 30));
 module.exports = vqv;
